481639bb280851cea7f741058837c615c4603497
nedia <aiden@nedia.dev>
2022-11-03 16:26:49 +1300

    feat: Add {nedots,sync} commands and {file,git} opts

--- src/cmd/nedots.go
+++ src/cmd/nedots.go
@@ -5,21 +5,84 @@ import (
 	"fmt"
 	"os"
 
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "nedots",
-	Short: "Manage (ne)dots - collect & sync dotfiles.",
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
-}
+var (
+	Verbosity  int
+	ConfigFile string
+	RootDir    string
+	rootCmd    = &cobra.Command{
+		Use:   "nedots",
+		Short: "Manage (ne)dots - collect & sync dotfiles.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello nedots!")
+		},
+	}
+)
 
-// Execute Run nedots
+// Execute Run nedots, print error to stdout?
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(readConfigFile)
+
+	rootCmd.PersistentFlags().CountVarP(
+		&Verbosity,
+		"verbosity",
+		"v",
+		"uage: -vv (none: regular, 1: more verbose, 2: debug)",
+	)
+	rootCmd.PersistentFlags().StringVarP(
+		&ConfigFile,
+		"config",
+		"c",
+		"",
+		"config file (default: $HOME/.nedots/.nedots.yml)",
+	)
+	rootCmd.PersistentFlags().StringVarP(
+		&RootDir,
+		"directory",
+		"d",
+		"",
+		"nedots directory (default: $HOME/.nedots)",
+	)
+
+	rootCmd.AddCommand(syncCmd)
+}
+
+func readConfigFile() {
+	if ConfigFile != "" {
+		// We've been passed a configuration file so no need to find it ourselves
+		viper.SetConfigFile(ConfigFile)
+	} else {
+		if RootDir != "" {
+			// We've been passed a directory that is different to the default
+			viper.AddConfigPath(RootDir)
+		} else {
+			// Otherwise, we'll find the home directory & check the default location
+			// Find home directory
+			home, err := homedir.Dir()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			viper.AddConfigPath(home + "/.nedots")
+		}
+
+		viper.SetConfigName(".nedots")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
 		os.Exit(1)
 	}
 }
--- src/cmd/sync.go
+++ src/cmd/sync.go
@@ -1,1 +1,30 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	Only    []string
+	syncCmd = &cobra.Command{
+		Use:   "sync",
+		Short: "Sync files [(ne)dots] with remote",
+		Long: `Sync files [(ne)dots] with remote.
+    Fastforward and push latest changes to remote.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello sync!")
+		},
+	}
+)
+
+func init() {
+	syncCmd.PersistentFlags().StringSliceVarP(
+		&Only,
+		"only",
+		"o",
+		[]string{},
+		"only sync these entries (must match config)",
+	)
+}
